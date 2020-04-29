83d283b54e8b2d6edcc9e8a25f0235d5e3bd0aa7
Anastasia <Anastasia.beetroot@gmail.com>
2020-04-29 13:50:20 +0300

    added function

--- stats.php
+++ stats.php
@@ -1,128 +1,9 @@
 <?php
-define('NEW_LINE','<br>');
-define('SURNAME_MERKEL', 'Merkel');
-define('JACK_NAME', 'Jack');
-$users = [
-    [
-        'name' => 'Bob',
-        'surname' => 'Martin',
-        'age' => 75,
-        'gender' => 'man',
-        'avatar' => 'https://i.ytimg.com/vi/sDnPs_V8M-c/hqdefault.jpg',
-        'animals' => ['dog']
-    ],
-    [
-        'name' => 'Alice',
-        'surname' => 'Merton',
-        'age' => 25,
-        'gender' => 'woman',
-        'avatar' => 'https://i.scdn.co/image/d44a5d71596b03b5dc6f5bbcc789458700038951',
-        'animals' => ['dog', 'cat']
-    ],
-    [
-        'name' => 'Jack',
-        'surname' => 'Sparrow',
-        'age' => 45,
-        'gender' => 'man',
-        'avatar' => 'https://pbs.twimg.com/profile_images/427547618600710144/wCeLVpBa_400x400.jpeg',
-        'animals' => []
-    ],
-    [
-        'name' => 'Angela',
-        'surname' => 'Merkel',
-        'age' => 65,
-        'gender' => 'woman',
-        'avatar' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Besuch_Bundeskanzlerin_Angela_Merkel_im_Rathaus_K%C3%B6ln-09916.jpg/330px-Besuch_Bundeskanzlerin_Angela_Merkel_im_Rathaus_K%C3%B6ln-09916.jpg',
-        'animals' => ['dog', 'parrot', 'horse']
-    ]
-];
-if (!empty($_POST)){
-    $users[] = $_POST;
-    }
-$ages = array_column($users,'age');
-var_dump($ages);
-$maxAge = max($ages);
-$maxAgeId = array_search($maxAge,$ages);
-echo "<br>";
-var_dump($maxAgeId);
-$oldestUser = $users[$maxAgeId];
-$userName = array_column($users, 'name');
-var_dump($userName);
-$userJackId = array_search (JACK_NAME, $userName);
-print_r($users[$userJackId]) ;
-echo "<br>";
-$randomUserId = rand(0, count($users) -1);
-$randomUser = $users[$randomUserId];
-echo NEW_LINE;
-$userSurname = array_column($users, 'surname');
-$userMerkelId = array_search (SURNAME_MERKEL, $userSurname);
-$merkelAnimals = $users[$userMerkelId]['animals'];
-asort($merkelAnimals);
-print_r($merkelAnimals);
-echo NEW_LINE;
-$maxAgeAllId = array_keys($ages,$maxAge);
-//$oldUsers = array();
- foreach ($maxAgeAllId as $oldUsers)
-{ echo $users[$oldUsers]['name']. ",". " " ;
-}
-echo NEW_LINE;
- if (!empty($_GET['sort'])){
- switch  ($_GET['sort']){
-     case 'id':
-         if (!empty($_GET['order'])&& $_GET['order'] == 'desc'){
-             krsort($users);
-         }else{
-             ksort($users);
-         }
-         $users = array_values($users);
-         break;
- }
-}
- $animals  = [];
- foreach ($users as $user) {
-     $animals = array_merge($animals, $user['animals']);
-   }
- $animalsFilter = array_unique($animals);
- //print_r($animalsFilter);
-if (!empty($_GET['filter'])){
-    switch($_GET['filter']){
-        case 'man':
-            foreach ($users as $key => $user){
-                if ($user['gender']!== 'man'){
-                    unset($users[$key]);
-                }
-            }
-            break;
-        case 'woman':
-            foreach ($users as $key => $user) {
-                if ($user['gender'] !== 'woman') {
-                    unset($users[$key]);
-                }
-            }
-            break;
-        case 'covid':
-            foreach ($users as $key => $user) {
-            if ($user['age'] < 60 ) {
-                unset($users[$key]);
-            }
-        }
-            break;
-        case 'dog':
-        case 'cat':
-        case 'parrot':
-        case 'horse':
-            foreach ($users as $key=> $user){
-                $index = array_search($_GET['filter'], $user['animals']);
-                if (false==$index){
-                    unset ($users[$key]);
-                }
-            }
-            break;
-    }
-}
+error_reporting(E_ALL);
+ini_set('display_errors',true);
 
+require './functions.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -176,7 +57,7 @@ if (!empty($_GET['filter'])){
         <tbody>
 
         <?php foreach ($users as $key => $user): ?>
-        <?php $id = !empty($_GET['sort'] && $_GET['sort']== 'id' && $_GET['order'] == 'desc') ? count($users) - $key: $key +1; ?>
+        <?php $id = !empty($_GET['sort']) && $_GET['sort']== 'id' && $_GET['order'] == 'desc' ? count($users) - $key: $key +1; ?>
         <tr style="background-color: <?=($key%2 === 0) ? '#aaa':'#fff' ?>">
             <td><?=$id?></td>
             <td><?=$user['name']?></td>
